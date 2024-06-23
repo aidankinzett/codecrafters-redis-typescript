1d749305b378035e9cf0de8a9e856acd54ac1726
Aidan Kinzett <aidan@labrys.io>
2024-06-23 21:32:36 +1000

    codecrafters submit [skip ci]

--- app/commandHandlers/infoHandler.ts
+++ app/commandHandlers/infoHandler.ts
@@ -3,5 +3,14 @@ import { bulkString } from "../RESP/bulkString";
 import { config } from "../config";
 
 export const infoHandler: RedisCommandHandler = (connection, data) => {
-  connection.write(bulkString(`role:${config.role}`));
+  const output = bulkString(
+    [
+      `role:${config.role}`,
+      "master_replid:8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb",
+      "master_repl_offset:0",
+    ].join("\r\n"),
+  );
+
+  console.log(output);
+  connection.write(output);
 };
